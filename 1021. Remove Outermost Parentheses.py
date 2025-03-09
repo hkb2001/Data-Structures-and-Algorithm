dc151f071f188369e0d219cef5ee90937f8d9f0b
hkb2001 <hasnainkbhatti@gmail.com>
2025-03-10 01:39:02 +0300

    push 1021

---
+++ 1021. Remove Outermost Parentheses.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/remove-outermost-parentheses/description/
+
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        res, opened = [], 0
+        for c in S:
+            if c == '(' and opened > 0: 
+                res.append(c)
+            if c == ')' and opened > 1: 
+                res.append(c)
+            opened += 1 if c == '(' else -1
+        
+        return "".join(res)
