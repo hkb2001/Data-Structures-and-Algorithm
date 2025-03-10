94f82e08ab0c55d580d2825136fe2c748ae887fd
hkb2001 <hasnainkbhatti@gmail.com>
2025-03-10 23:15:55 +0300

    push 26 updated

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -12,3 +12,16 @@ class Solution:
             nums[i] = arr[i]
         
         return len(arr)
+    
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        i = 0  
+        for j in range(1, len(nums)):  # Fast pointer
+            if nums[j] != nums[i]:  # Found a new unique element
+                i += 1  # Move slow pointer
+                nums[i] = nums[j]  # Update array in-place
+        
+        return i + 1  # Length of unique elements
