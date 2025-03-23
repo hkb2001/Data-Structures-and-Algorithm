e6e7a7c81d972800f113dc85f645cc05a6457750
hkb2001 <hasnainkbhatti@gmail.com>
2025-03-23 16:58:14 +0300

    push 2396

---
+++ 2396. Strictly Palindromic Number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/strictly-palindromic-number/description/
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+        # def convert(n,b):
+        #     s=""
+        #     while n:
+        #         s+=str(n%b)
+        #         n//=b
+        #     return s
+        # for b in range(2, n-1):
+        #     s = convert(n,b)
+        #     if s!=s[::-1]:
+        #         return False
+        # return True
+        
