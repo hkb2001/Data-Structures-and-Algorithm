2835c47cab4aff148a0014267f95abe608ae8169
hkb2001 <hasnainkbhatti@gmail.com>
2025-03-23 17:11:39 +0300

    push 1528

--- 2396. Strictly Palindromic Number.py
+++ 2396. Strictly Palindromic Number.py
@@ -2,15 +2,20 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
         return False
-        # def convert(n,b):
-        #     s=""
-        #     while n:
-        #         s+=str(n%b)
-        #         n//=b
-        #     return s
-        # for b in range(2, n-1):
-        #     s = convert(n,b)
-        #     if s!=s[::-1]:
-        #         return False
-        # return True
+
+# Solution 2
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def convert(n,b):
+            s=""
+            while n:
+                s+=str(n%b)
+                n//=b
+            return s
+        for b in range(2, n-1):
+            s = convert(n,b)
+            if s!=s[::-1]:
+                return False
+        return True
         
