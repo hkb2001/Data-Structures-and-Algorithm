e223ed92973da351a4eae163dc769d4187e59149
hkb2001 <hasnainkbhatti@gmail.com>
2025-03-23 17:12:15 +0300

    push 1528

---
+++ 1528. Shuffle String.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/shuffle-string/
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = [''] * len(s)
+        for i in range(len(s)):
+            result[indices[i]] = s[i]
+        return "".join(result) 
+        
